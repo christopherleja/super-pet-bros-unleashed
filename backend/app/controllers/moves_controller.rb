1861853a390f124575a5010a13ebf9aa788b9da4
Chris Leja <christopher.leja@gmail.com>
2020-04-11 16:09:19 -0400

    added move and pet controllers plus routes

---
+++ backend/app/controllers/moves_controller.rb
@@ -0,0 +1,15 @@
+class MovesController < ApplicationController
+
+    def index
+        moves = Move.all
+
+        render json: moves
+    end
+
+    def show
+        move = Move.find(params[id])
+
+        render json: move
+    end
+    
+end
