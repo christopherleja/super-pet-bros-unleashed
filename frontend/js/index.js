14f85687b492f3a3284c3f838788632d9355c8e3
Chris Leja <christopher.leja@gmail.com>
2020-04-13 12:09:54 -0400

    built function to render battle page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -119,41 +119,6 @@ function renderBattle(playerPet, petObj){
     petBattleDiv.append(opponentDiv, playerPetDiv)
 
 }
-//     petDetailDiv.style.display = "block"
-//     let effectArray = ["none", "reduces opponent's defense", "increases defense", "lowers opponent's speed", "increases speed", "lowers opponenent's attack", "increases attack", "restores hp"]
-    
-//     petDetailDiv.innerHTML = `
-//     <img class="pet-image" src="${petObj['pet-image-url']}"><br />
-//     <h2 id="pet-name">${petObj.name}</h2>
-//     <ul class="pet-stat-ul">
-//         <li>HP: ${petObj.hp}</li>
-//         <li>Attack: ${petObj.attack}</li>
-//         <li>Defense: ${petObj.defense}</li>
-//         <li>Speed: ${petObj.speed}</li>
-//     </ul>
-//     <ol class="pet-move-ol">
-//         <li>${petObj.moves[0].name}</li>
-//             <ul class="move-stats-ul">
-//                 <li>Power: ${petObj.moves[0].power}</li>
-//                 <li>Effect: ${effectArray[petObj.moves[0].effect_target]} by ${petObj.moves[0].effect}%</li>
-//             </ul>
-//         <li>${petObj.moves[1].name}</li>
-//             <ul class="move-stats-ul">
-//                 <li>Power: ${petObj.moves[1].power}</li>
-//                 <li>Effect: ${effectArray[petObj.moves[1].effect_target]} by ${petObj.moves[1].effect}%</li>
-//             </ul>
-//         <li>${petObj.moves[2].name}</li>
-//             <ul class="move-stats-ul">
-//                 <li>Power: ${petObj.moves[2].power}</li>
-//                 <li>Effect: ${effectArray[petObj.moves[2].effect_target]} by ${petObj.moves[2].effect}%</li>
-//             </ul>
-//         <li>${petObj.moves[3].name}</li>
-//             <ul class="move-stats-ul">
-//                 <li>Power: ${petObj.moves[3].power}</li>
-//                 <li>Effect: ${effectArray[petObj.moves[3].effect_target]} by ${petObj.moves[3].effect}%</li>
-//             </ul>
-//     `
-// // }
 
 createTab.addEventListener("click", () => {
   createPet()
