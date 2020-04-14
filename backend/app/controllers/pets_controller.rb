b26906d99e2c2891bea9b64b0a5c15ea202a42af
erocknine <echeungsk@gmail.com>
2020-04-14 10:50:35 -0400

    create update

--- backend/app/controllers/pets_controller.rb
+++ backend/app/controllers/pets_controller.rb
@@ -13,14 +13,15 @@ class PetsController < ApplicationController
     end
 
     def create
-        pet = Pet.create(create_pet_params)
+        pet = Pet.create(name: params[:name], pet_image_url: params[:pet_image_url], hp: params[:hp], attack: params[:attack], defense: params[:defense], speed: params[:speed])
+        params[:moves].each{|move| Joiner.create(pet_id: pet.id, move_id: move[:id])}
         render json: pet
     end
 
     private
 
-    def create_pet_params
-        params.permit(:name, :pet_image_url, :hp, :attack, :defense, :speed, :moves)
-    end
+    # def create_pet_params
+    #     params.permit(:name, :pet_image_url, :hp, :attack, :defense, :speed, :moves)
+    # end
     
 end
